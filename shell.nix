22028ac226b4153a3d2db989fd4f169d2ba9f57a
Abhishek Shah <abhishekshah3@gmail.com>
2025-05-06 21:38:08 +0530

    update nix-shell

--- shell.nix
+++ shell.nix
@@ -5,12 +5,10 @@ pkgs.mkShell {
     pkgs.zig
     pkgs.zls
     pkgs.clang
-    pkgs.zsh
     pkgs.pkg-config
   ];
 
   shellHook = ''
-    export SHELL=${pkgs.zsh}/bin/zsh
-    exec ${pkgs.zsh}/bin/zsh
+    export SHELL=/usr/bin/zsh
   '';
 }
