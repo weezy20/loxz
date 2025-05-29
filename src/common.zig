2c99b4970aa4fc30f9afe5374e1a49ef6fcc2e62
Abhishek Shah <abhishekshah3@gmail.com>
2025-05-29 12:41:17 +0530

    Comments

--- src/common.zig
+++ src/common.zig
@@ -1,6 +1,6 @@
 const lib = @import("root.zig");
 const build_options = @import("build_options");
 /// Default hasher for keys in the table
-// Conditionally use clhash..
 pub const hasher = lib.loxHash;
+/// Use lib.hasClhash to determine if CLHash is available
 pub const clhasher = lib.ClHash.hash;
